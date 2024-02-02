f603b94bff00e98198325038101b19a9abb2b976
Jagan2412 <42362204+Jagan2412@users.noreply.github.com>
2024-02-02 17:53:44 +0530

    Add files via upload

---
+++ C#/GenericClassExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    // Generic class to represent a pair of values
+    public class Pair<T1, T2>
+    {
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+    }
+
+    public class GenericClassExample
+    {
+        public static void Main()
+        {
+            Pair<string, int> person = new Pair<string, int>
+            {
+                First = "John",
+                Second = 30
+            };
+
+            Console.WriteLine($"Name: {person.First}, Age: {person.Second}");
+        }
+    }
+}
---
+++ C#/GenericExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    public class GenericExample
+    {
+        // Generic method to swap two values
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Main()
+        {
+            int x = 5, y = 10;
+            Console.WriteLine($"Before swapping: x = {x}, y = {y}");
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"After swapping: x = {x}, y = {y}");
+
+            string str1 = "Hello", str2 = "World";
+            Console.WriteLine($"Before swapping: str1 = {str1}, str2 = {str2}");
+
+            Swap(ref str1, ref str2);
+
+            Console.WriteLine($"After swapping: str1 = {str1}, str2 = {str2}");
+        }
+    }
+}
