12d4ecda1fc9513609c64ce8e5cf22489afcd8c6
Jagan2412 <42362204+Jagan2412@users.noreply.github.com>
2024-02-20 18:58:13 +0530

    Add files via upload

---
+++ Oops/AbstractClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    // Abstract class
+    class AbstractClass
+    {
+        static void Main()
+        {
+            // Creating instances of derived classes
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+
+            // Calling abstract method and concrete method
+            circle.Draw();
+            circle.DisplayArea();
+            Console.WriteLine($"Circle area: {circle.CalculateArea()}");
+
+            Console.WriteLine();
+
+            // Calling abstract method and overridden method
+            rectangle.Draw();
+            rectangle.DisplayArea(); // Calls the overridden method in Rectangle
+        }
+    }
+
+    public abstract class Shape
+    {
+        // Abstract method (to be implemented by derived classes)
+        public abstract void Draw();
+
+        // Concrete method
+        public void DisplayArea()
+        {
+            Console.WriteLine("Area calculation not implemented for this shape.");
+        }
+    }
+
+    // Derived class (inherits from Shape)
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        // Implementation of the abstract method
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a circle with radius {radius}");
+        }
+
+        // Additional method specific to Circle
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    // Derived class (inherits from Shape)
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        // Implementation of the abstract method
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a rectangle with length {length} and width {width}");
+        }
+
+        // Implementation of the DisplayArea method for rectangles
+        public new void DisplayArea()
+        {
+            Console.WriteLine($"Area of the rectangle: {length * width}");
+        }
+    }
+
+}
---
+++ Oops/InterfaceAbstraction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class InterfaceAbstraction
+    {
+        static void Main()
+        {
+            // Creating an instance of the Circle class
+            Circle circle = new Circle(5);
+
+            // Calling methods through interfaces
+            ((IDrawable)circle).Draw();
+            Console.WriteLine($"Circle area: {((ICalculatable)circle).CalculateArea()}");
+        }
+    }
+
+    // Interface
+    public interface IDrawable
+    {
+        void Draw();
+    }
+
+    // Another interface
+    public interface ICalculatable
+    {
+        double CalculateArea();
+    }
+
+    // Class implementing both interfaces
+    public class Circle : IDrawable, ICalculatable
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        // Implementation of the Draw method from IDrawable
+        public void Draw()
+        {
+            Console.WriteLine($"Drawing a circle with radius {radius}");
+        }
+
+        // Implementation of the CalculateArea method from ICalculatable
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+
+
+}
