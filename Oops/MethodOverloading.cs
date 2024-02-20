732c02c1e30cf52276f32ef10fcbeacc80927971
Jagan2412 <42362204+Jagan2412@users.noreply.github.com>
2024-02-20 17:33:44 +0530

    Add files via upload

---
+++ Oops/MethodOverloading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+
+    class MethodOverloading
+    {
+        static void Main()
+        {
+            Calculator calculator = new Calculator();
+
+            int result1 = calculator.Add(5, 10);
+            double result2 = calculator.Add(3.5, 7.2);
+            string result3 = calculator.Add("Hello, ", "World!");
+
+            Console.WriteLine(result1); // 15
+            Console.WriteLine(result2); // 10.7
+            Console.WriteLine(result3); // Hello, World!
+        }
+    }
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public string Add(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+    }
+
+}
---
+++ Oops/MethodOverride.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+
+    class MethodOverride
+    {
+        static void Main()
+        {
+            Shape shape1 = new Circle();
+            Shape shape2 = new Square();
+
+            shape1.Draw(); // Drawing a circle
+            shape2.Draw(); // Drawing a square
+        }
+    }
+
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+        }
+    }
+
+    class Square : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a square");
+        }
+    }
+
+}
